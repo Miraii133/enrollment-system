3740a75bb73d41056fd60d2872d457a08fa08c86
Jiyo <jiyovalmoria@gmail.com>
2022-08-17 21:55:25 +0800

    add: jframe properties.

--- Exer2/src/exer2/Exer2.java
+++ Exer2/src/exer2/Exer2.java
@@ -14,7 +14,8 @@ public class Exer2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       StudentsJFrame studentsFrame = new StudentsJFrame();
+       studentsFrame.setVisible(true);
     }
     
 }
