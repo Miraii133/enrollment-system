1a24e0bc6c22303e39dcc761a17d83d3c4579d8c
Jiyo <jiyovalmoria@gmail.com>
2022-09-28 12:56:18 +0800

    add: exer4, newDB menu, getCurrentYear, nextYear.

---
+++ Exer4/src/exer4/Exer4.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exer4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author jiyo
+ */
+
+class sample {
+    
+}
+public class Exer4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       StudentsJFrame studentsFrame = new StudentsJFrame();
+       studentsFrame.setVisible(true);
+       
+
+       }
+       
+       
+    
+    
+}
